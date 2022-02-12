1b55eceef047cd85a6c1508b30a8fc5ded0131c0
Kirk Easterson <kirk.easterson@gmail.com>
2022-02-12 20:57:35 +0000

    Added more to script for setting display settings

--- display_settings.sh
+++ display_settings.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# Replace 9 with the id of the device from the command 'xinput'
-xinput --set-int-prop 9 "libinput Natural Scrolling Enabled" 8 1
+# Replace ID with the id of the device from the command 'xinput'
+xinput --set-int-prop ID "libinput Natural Scrolling Enabled" 8 1
+
+# Set tap to click for touchpad, replace ID with id fro ''xinput'
+xinput set-prop ID "libinput Tapping Enabled" 1
 
